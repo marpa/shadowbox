b25eb695a65dd78a41bae635defd78b6dbc9ee7f
marpa <achapin@gmail.com>
2009-10-08 18:52:07 -0400

    added config file so admins can config ShadowBox and update as needed

---
+++ config-sample.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Configuration otions for ShadowBox theme
+ */
+
+
+$shadowbox_config = array();
+
+$shadowbox_config['meta_left_options']['blogs'] = array (
+		option_name =>	'blogs',
+		option_label =>	'Blog Sign Up',
+		option_value => ''
+	);
+	
+$shadowbox_config['meta_left_options']['custom'] = array (
+		option_name =>	'custom',
+		option_label =>	'Custom',
+		option_value => ''
+	);
+
+		
