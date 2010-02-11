52cfe2357ed01b268fbfec5bc48cb56f555159f3
marpa <achapin@gmail.com>
2010-02-11 15:04:33 -0500

    added model options for IE CSS opacity

--- functions.php
+++ functions.php
@@ -392,6 +392,13 @@ function shadowbox_options() {
 			padding: 1px;
 		}
 
+		".$options['header-color-ie']."; 
+		".$options['top-color-ie']."; 
+		".$options['content-color-ie']."; 
+		".$options['bottom-color-ie']."; 
+		".$options['left01-color-ie']."; 
+		".$options['right01-color-ie']."; 
+		".$options['right02-color-ie'].";
 
  	
  	</style>";	 
