8e34a023993eae94f863b0a04f95bfc9cfe69f2b
marpa <achapin@gmail.com>
2010-02-11 14:25:51 -0500

    renamed topbar.php and bottombar.php to sidebar-bottom.php and sidebar-top.php

---
+++ sidebar-bottom.php
@@ -0,0 +1,10 @@
+<?php 	/* Widgetized sidebar, if you have the plugin installed. */
+	if ( !function_exists('dynamic_sidebar') || !dynamic_sidebar('sidebar-5') ) : ?>
+
+
+<?php endif; ?>
+
+
+
+	
+
