e3b237838dc5e7260a4cd00b6b173a5a815cd0d2
marpa <achapin@gmail.com>
2010-02-11 14:24:03 -0500

    added template for 2nd right sidebar

---
+++ sidebar-right02.php
@@ -0,0 +1,35 @@
+<?php
+/**
+ * @package WordPress
+ * @subpackage ShadowBox
+ */
+?>
+<div class="sidebarright02">
+
+	<?php
+	// display link to new post if user is at least an author
+	if (wp_get_current_user()->user_level > 7) {
+		print "<div class='editlink'>";
+		print "<a href='". get_bloginfo('url')."/wp-admin/widgets.php?show=&amp;sidebar=sidebar-3'>Edit Widgets</a>";
+		print "</div>"; 
+	}?>
+	
+	<ul>
+		<?php 	/* Widgetized sidebar, if you have the plugin installed. */
+		if ( !function_exists('dynamic_sidebar') || !dynamic_sidebar('sidebar-3') ) : ?>
+		
+			<li><h2>Archives</h2>
+				<ul>
+				<?php wp_get_archives('type=monthly'); ?>
+				</ul>
+			</li>
+		
+			<?php /* If this is the frontpage */ if ( is_home() || is_page() ) { ?>
+				<?php wp_list_bookmarks(); ?>
+
+		<?php } ?>	
+		<?php endif; ?>
+	</ul>
+</div>
+
+
