adeb0f418b28924644c036384c740ae88194b479
marpa <achapin@gmail.com>
2010-02-22 20:42:12 -0500

    updated css

--- 404.php
+++ 404.php
@@ -7,12 +7,16 @@
 get_header();
 ?>
 
-	<div id="content" class="centercontent">
+	<div id="content" class="contentblock">
 
 		<h2 class="center">Error 404 - Not Found</h2>
+		<p style="padding-bottom: 100px;"> The page you are looking for is not here....<br/><br/>  
+		
+		Click on the header above to go to the main page of this blog
+		</p>
 
 	</div>
 
-<?php get_sidebar(); ?>
+<?php// get_sidebar(); ?>
 
 <?php get_footer(); ?>
