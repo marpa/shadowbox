9ea5c6f53f3ec200d864854a9cedeeb889f05e22
marpa <achapin@gmail.com>
2010-03-03 17:06:17 -0500

    Changed the scope of the entry div tag so it was limited to the post content and does not include postmetadata

--- single.php
+++ single.php
@@ -35,48 +35,48 @@ if ($options['post-sidebar-left-display'] == "show" && $options['left01-width'] 
 			Categories: <span class='category'><?php the_category('</span> <span class=\'category\'>') ?></span>
 		</div>
 
-			<small><?php the_time('l, F jS, Y') ?>  by <?php the_author_posts_link(); ?></small>
-			<div class="entry">
-			
-				<?php the_content('<div class=\'morelink\'>&laquo; More &raquo;</div>'); ?> 
+		<small><?php the_time('l, F jS, Y') ?>  by <?php the_author_posts_link(); ?></small>
+		<div class="entry">
+		
+			<?php the_content('<div class=\'morelink\'>&laquo; More &raquo;</div>'); ?> 
 
-				<?php wp_link_pages(array('before' => '<p><strong>Pages:</strong> ', 'after' => '</p>', 'next_or_number' => 'number')); ?>
+			<?php wp_link_pages(array('before' => '<p><strong>Pages:</strong> ', 'after' => '</p>', 'next_or_number' => 'number')); ?>
 
-				<?php edit_post_link('Edit', '<div class="editlink">', '</div>'); ?>
-
-				<div class="postmetadata">
-					<?php the_tags('Tags: <span class=\'tag\'>','</span> <span class=\'tag\'>', '</span><br/>',' '); ?>
-				</div>
-
-				<div class="postmetadata alt">
-						
-						<?php if (('open' == $post-> comment_status) && ('open' == $post->ping_status)) {
-							// Both Comments and Pings are open ?>
-							<a href="#respond">Respond</a> <a href="<?php trackback_url(); ?>" rel="trackback">Trackback</a>
-
-						<?php } elseif (!('open' == $post-> comment_status) && ('open' == $post->ping_status)) {
-							// Only Pings are Open ?>
-							<a href="<?php trackback_url(); ?> " rel="trackback">Trackback</a><br/>
-							Responses are currently closed.
-
-						<?php } elseif (('open' == $post-> comment_status) && !('open' == $post->ping_status)) {
-							// Comments are open, Pings are not ?>
-							<a href="#respond">Respond</a><br/>
-							Pinging is currently not allowed.
-
-						<?php } elseif (!('open' == $post-> comment_status) && !('open' == $post->ping_status)) {
-							// Neither Comments, nor Pings are open ?>
-							Both comments and pings are currently closed.
-
-						<?php } ?>
-
-				</div>
-				<div id="syndication">
-					<span><?php post_comments_feed_link('Comments RSS'); ?></span>
-				</div>
-
-			</div>
+			<?php edit_post_link('Edit', '<div class="editlink">', '</div>'); ?>
 		</div>
+
+		<div class="postmetadata">
+			<?php the_tags('Tags: <span class=\'tag\'>','</span> <span class=\'tag\'>', '</span><br/>',' '); ?>
+		</div>
+
+		<div class="postmetadata alt">
+				
+				<?php if (('open' == $post-> comment_status) && ('open' == $post->ping_status)) {
+					// Both Comments and Pings are open ?>
+					<a href="#respond">Respond</a> <a href="<?php trackback_url(); ?>" rel="trackback">Trackback</a>
+
+				<?php } elseif (!('open' == $post-> comment_status) && ('open' == $post->ping_status)) {
+					// Only Pings are Open ?>
+					<a href="<?php trackback_url(); ?> " rel="trackback">Trackback</a><br/>
+					Responses are currently closed.
+
+				<?php } elseif (('open' == $post-> comment_status) && !('open' == $post->ping_status)) {
+					// Comments are open, Pings are not ?>
+					<a href="#respond">Respond</a><br/>
+					Pinging is currently not allowed.
+
+				<?php } elseif (!('open' == $post-> comment_status) && !('open' == $post->ping_status)) {
+					// Neither Comments, nor Pings are open ?>
+					Both comments and pings are currently closed.
+
+				<?php } ?>
+
+		</div>
+		<div id="syndication">
+			<span><?php post_comments_feed_link('Comments RSS'); ?></span>
+		</div>
+
+	</div>
 
 	<?php comments_template(); ?>
 
